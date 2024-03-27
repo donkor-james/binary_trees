5bb8a345f2074b86ab4a685aa60c8ac5fe8d14be
Donkor James <jamesdonkor987@gmail.com>
2024-03-27 14:45:24 +0000

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
 #include <stdarg.h>
 
 /**
@@ -19,10 +20,14 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /* Printing helper function */
 void binary_tree_print(const binary_tree_t *tree);
 
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 #endif
