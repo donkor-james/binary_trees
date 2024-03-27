8340ff767be1759a52a3f1ec84bcf93b8d1b2485
Donkor James <jamesdonkor987@gmail.com>
2024-03-27 16:52:08 +0000

    updated

--- tests/3-main.c
+++ tests/3-main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "binary_trees.h"
+#include "../binary_trees.h"
 
 /**
  * main - Entry point
@@ -20,4 +20,3 @@ int main(void)
 	binary_tree_delete(root);
 	return (0);
 }
-
